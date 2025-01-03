6cde406e87c22db5eed28e84f1184c384abbdd00
SomtoAch <achusiogu.somto@gmail.com>
2025-01-03 21:50:48 +0100

    index.html modified to use image elements within div containers rather than a background image for the divs. Script.js added, logic for the buttons written out. The 3 buttons have eventListeners added to them. Clicking the play button starts and interval controlled function(incrementeconds) which starts the timer. The pause button stops the interval but does not clear the accumulated time. Pressing the stop button does reset the time to 0. Style.css modified.

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ A repository containing the code that goes into making a chrome extension. I wil
 - allow the user to store the time session and description, to view later
 
 For now I will focus on the first 4 features
+
+### 03/01/2025
+- Created and added images for the buttons
+- Added event listeners to each button, and created the relevant functions for each button
+The timer is now fully functional whilst the popup stays open. This is not optimal of course, the timer would be much more useful if it continued incrementing with the popup closed.
+
+#### Buttons display flow
+- start: playing=false, reset=true, pauseBTN and stopBTN hidden
+- playBTN pressed -> playing=true, reset=false, pauseBTN unhidden, playBTN hidden, stopBTN hidden
+- pauseBTN pressed -> playing=false, reset=false, playBTN unhidden, pauseBTN hidden, stopBTN unhidden
+- stopBTN pressed -> playing=false, reset=true, playBTN unhidden, pauseBTN hidden, stopBTN hidden
---
+++ script.js
@@ -0,0 +1,57 @@
+let playing = false;
+let reset = true;
+let total_time = 0;
+let seconds = 0;
+let minutes = 0;
+let intervalID = 0;
+const pauseBTN = document.getElementById('pauseBTN');
+const playBTN = document.getElementById('playBTN');
+const stopBTN = document.getElementById('stopBTN');
+const timerText = document.getElementById('timerText');
+
+window.onload = () => {
+    console.log('onload');
+    pauseBTN.classList.add('hide');
+    stopBTN.classList.add('hide');
+    playBTN.classList.remove('hide');
+    pauseBTN.addEventListener("click", pressPauseBTN);
+    playBTN.addEventListener("click", pressPlayBTN);
+    stopBTN.addEventListener("click", pressStopBTN);
+}
+
+function incrementSeconds(){
+    total_time += 1;
+    seconds = total_time % 60;
+    minutes = Math.floor(total_time / 60);
+    timerText.innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+function pressPlayBTN(){
+    console.log('play pressed');
+    intervalID = setInterval(incrementSeconds, 1000);
+    playing = true;
+    reset = false;
+    playBTN.classList.add("hide");
+    pauseBTN.classList.remove("hide");
+    stopBTN.classList.add("hide");
+    console.log('bottom reached');
+}
+
+function pressPauseBTN(){
+    clearInterval(intervalID);
+    playing = false;
+    reset=false;
+    pauseBTN.classList.add('hide');
+    playBTN.classList.remove('hide');
+    stopBTN.classList.remove('hide');
+}
+
+function pressStopBTN(){
+    total_time = 0;
+    seconds = total_time % 60;
+    minutes = Math.floor(total_time / 60);
+    timerText.innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    playing=false;
+    reset = true;
+    stopBTN.classList.add('hide');
+}
